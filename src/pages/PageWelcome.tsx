f540011c8a69a0ea09a3fed489964d45f644efed
Edward Tanguay on dell001 <edward@tanguay.info>
2023-02-01 02:44:20 +0100

    initial commit

---
+++ src/pages/PageWelcome.tsx
@@ -0,0 +1,11 @@
+export const PageWelcome = () => {
+	return (
+		<>
+			    <h2>Video Sample</h2>
+    <iframe width="360" height="315" src="https://www.youtube.com/embed/HlzAtIHFRk0" title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+		</>
+	);
+};

298d9ce7154c4fb6504d87af8d9244a0cd539033
Edward Tanguay on dell001 <edward@tanguay.info>
2023-02-01 03:06:59 +0100

    finished

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,10 +1,22 @@
 export const PageWelcome = () => {
 	return (
 		<>
+			<h2>Nützliche Tools</h2>
+			<p><a href="https://www.npmjs.com/package/sweetstack">Sweetstack</a></p>
+
+			<h2>Hilfsmittel im Internet</h2>
+			<p><a href="https://google.com">Google</a></p>
+			<h2>Meine Webentwickler Sites</h2>
+			<p><a href="https://edwardtanguay.vercel.app/howtos">Edward's Howtos</a></p>
+			<p><a href="https://starters.tanguay.eu">Edward's Starter Sites</a></p>
+			<p><a href="https://jobs.tanguay.eu">Edward's Webentwickler Jobs in Deutschland</a></p>
+
 			    <h2>Video Sample</h2>
     <iframe width="360" height="315" src="https://www.youtube.com/embed/HlzAtIHFRk0" title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>
+			<h2>Webentwicklung leicht gemacht</h2>
+			<p><a href="https://ltn001.netlify.app">Zur HTML/CSS/JavaScript Site</a></p>
 		</>
 	);
 };
